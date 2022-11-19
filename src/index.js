f12ce3d8bef889ef9009b9283b76682c2f2b57b2
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-19 19:43:33 +0200

    'update'

--- src/index.js
+++ src/index.js
@@ -2,5 +2,38 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import galleryCard from './templates/galleryCard.hbs';
 
+const searchFormRef = document.querySelector('#search-form');
+const galleryContainerRef = document.querySelector('.gallery');
+const inputRef = document.querySelector('[name="searchQuery"]');
+
+searchFormRef.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+   e.preventDefault();
+
+   const form = e.currentTarget;
+   const searchQuery = form.elements.searchQuery.value;
+   
+   fetchImage(searchQuery).then(renderGalleryCard).catch(onFetchError);
+}
+
+function fetchImage(searchQuery) {
+   const url = `https://pixabay.com/api/?key=31422890-5e40c603f0e6080de62657891&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+   return fetch(url).then(res => res.json());
+}
+
+function renderGalleryCard(value) {
+   console.log(value.hits);
+   const markup = galleryCard(value.hits);
+   galleryContainerRef.innerHTML = markup;
+}
+
+function onFetchError() {
+   Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+}
+
+let lightbox = new SimpleLightbox('.gallery a');
 
---
+++ src/js/getImages.js
@@ -0,0 +1,6 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '31422890-5e40c603f0e6080de62657891';
+
+function getImages() {
+
+}
