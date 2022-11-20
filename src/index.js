7253be09ebcf5c48aee03da8479858e15863c967
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-20 21:25:45 +0200

    'update'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import galleryCard from './templates/galleryCard.hbs';
--- src/js/getImages.js
+++ src/js/getImages.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '31422890-5e40c603f0e6080de62657891';
 
@@ -6,7 +8,7 @@ export class ImagesAPI {
    static currentPage = 1;
    static query = '';
 
-   static findImages(searchQuery) {
+   static async findImages(searchQuery) {
       if (searchQuery !== undefined) {
          ImagesAPI.query = searchQuery;
          ImagesAPI.currentPage = 1;
@@ -14,24 +16,24 @@ export class ImagesAPI {
          ImagesAPI.currentPage += 1;
       }
 
-      const params = new URLSearchParams({
-         'q': ImagesAPI.query,
-         'image_type': 'photo',
-         'orientation': 'horizontal',
-         'safesearch': 'true',
-         'per_page': 40,
-         'page': ImagesAPI.currentPage
-      });
-      
-      const url = `${BASE_URL}?key=${API_KEY}&${params}`;
-   
-      return fetch(url).then(response =>{
-         if (response.ok) {
-            return response.json();
-          } else {
-             throw new Error(response.status);
-          }
-      });
+      const options = {
+         params: {
+            'key': API_KEY,
+            'q': ImagesAPI.query,
+            'image_type': 'photo',
+            'orientation': 'horizontal',
+            'safesearch': 'true',
+            'per_page': 40,
+            'page': ImagesAPI.currentPage
+         }
+      }
+
+      try {
+         const response = await axios.get(`${BASE_URL}`, options);
+         return response.data;
+       } catch (error) {
+         console.log(error);
+       }
    }
 }
 
