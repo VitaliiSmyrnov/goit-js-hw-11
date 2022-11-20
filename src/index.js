91916f5bffca8e54dcb287d36c4eeab7048c11c1
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-20 11:52:15 +0200

    'update'

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import galleryCard from './templates/galleryCard.hbs';
+import { ImagesAPI } from './js/getImages.js';
 
 const searchFormRef = document.querySelector('#search-form');
 const galleryContainerRef = document.querySelector('.gallery');
-const inputRef = document.querySelector('[name="searchQuery"]');
+const btnLoad = document.querySelector('.load-more');
 
 searchFormRef.addEventListener('submit', onSearch);
 
@@ -16,46 +17,51 @@ function onSearch(e) {
    const form = e.currentTarget;
    const searchQuery = form.elements.searchQuery.value;
    
-   findImages(searchQuery).then(renderGalleryCard).catch(onFetchError);
+   ImagesAPI.findImages(searchQuery).then(renderGalleryCard).catch(onSearchError);
 }
 
-function findImages(searchQuery) {
-   const BASE_URL = 'https://pixabay.com/api/';
-   const API_KEY = '31422890-5e40c603f0e6080de62657891';
-   const params = new URLSearchParams({
-      'q': searchQuery,
-      'image_type': 'photo',
-      'orientation': 'horizontal',
-      'safesearch': 'true',
-      'per_page': 40,
-      'page': 1
-   });
+// function findImages(searchQuery) {
+//    const BASE_URL = 'https://pixabay.com/api/';
+//    const API_KEY = '31422890-5e40c603f0e6080de62657891';
+//    const params = new URLSearchParams({
+//       'q': searchQuery,
+//       'image_type': 'photo',
+//       'orientation': 'horizontal',
+//       'safesearch': 'true',
+//       'per_page': 40,
+//       'page': 1
+//    });
    
-   const url = `${BASE_URL}?key=${API_KEY}&${params}`;
+//    const url = `${BASE_URL}?key=${API_KEY}&${params}`;
 
-   return fetch(url).then(response =>{
-      if (response.ok) {
-         return response.json();
-       } else {
-          throw new Error(response.status);
-       }
-   });
-}
+//    return fetch(url).then(response =>{
+//       if (response.ok) {
+//          return response.json();
+//        } else {
+//           throw new Error(response.status);
+//        }
+//    });
+// }
 
 function renderGalleryCard(value) {
    // console.log(value);
    if (value.hits.length === 0) {
       return Notify.info('Sorry, there are no images matching your search query. Please try again.');
    }
-   
    Notify.success(`Hooray! We found ${value.totalHits} images.`)
+   galleryContainerRef.innerHTML = '';
    const markup = galleryCard(value.hits);
    galleryContainerRef.innerHTML = markup;
+   btnLoad.classList.remove('visually-hidden');
 }
 
-function onFetchError(error) {
+function onSearchError(error) {
    Notify.failure(error);
 }
+
+btnLoad.addEventListener('click', ()=>{
+   ImagesAPI.findImages().then(renderGalleryCard).catch(onSearchError);
+})
 
 let lightbox = new SimpleLightbox('.gallery a');
 
--- src/js/getImages.js
+++ src/js/getImages.js
@@ -1,6 +1,39 @@
 const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '31422890-5e40c603f0e6080de62657891';
+const API_KEY = '31422890-5e40c603f0e6080de62657891';
 
-function getImages() {
+export class ImagesAPI {
+   static currentPage = 1;
+   static searchQuery = '';
 
+   static findImages(searchQuery) {
+      if (searchQuery !== undefined) {
+         ImagesAPI.searchQuery = searchQuery;
+         ImagesAPI.currentPage = 1;
+      } else {
+         ImagesAPI.currentPage += 1;
+      }
+
+
+      // const BASE_URL = 'https://pixabay.com/api/';
+      // const API_KEY = '31422890-5e40c603f0e6080de62657891';
+      const params = new URLSearchParams({
+         'q': ImagesAPI.searchQuery,
+         'image_type': 'photo',
+         'orientation': 'horizontal',
+         'safesearch': 'true',
+         'per_page': 40,
+         'page': ImagesAPI.currentPage
+      });
+      
+      const url = `${BASE_URL}?key=${API_KEY}&${params}`;
+   
+      return fetch(url).then(response =>{
+         if (response.ok) {
+            return response.json();
+          } else {
+             throw new Error(response.status);
+          }
+      });
+   }
 }
+
