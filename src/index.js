7f8a81998703928aa4c08bff2d43a68a3cf69781
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-20 15:42:39 +0200

    'update'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import galleryCard from './templates/galleryCard.hbs';
 import { ImagesAPI } from './js/getImages.js';
 
@@ -12,56 +12,62 @@ const btnLoad = document.querySelector('.load-more');
 searchFormRef.addEventListener('submit', onSearch);
 
 function onSearch(e) {
-   e.preventDefault();
+  e.preventDefault();
 
-   const form = e.currentTarget;
-   const searchQuery = form.elements.searchQuery.value;
-   
-   ImagesAPI.findImages(searchQuery).then(renderGalleryCard).catch(onSearchError);
+  const form = e.currentTarget;
+  const searchQuery = form.elements.searchQuery.value;
+
+  ImagesAPI.findImages(searchQuery)
+    .then(data => {
+      Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      galleryContainerRef.innerHTML = '';
+      renderGalleryCard(data);
+    })
+    .catch(onSearchError);
 }
 
-// function findImages(searchQuery) {
-//    const BASE_URL = 'https://pixabay.com/api/';
-//    const API_KEY = '31422890-5e40c603f0e6080de62657891';
-//    const params = new URLSearchParams({
-//       'q': searchQuery,
-//       'image_type': 'photo',
-//       'orientation': 'horizontal',
-//       'safesearch': 'true',
-//       'per_page': 40,
-//       'page': 1
-//    });
-   
-//    const url = `${BASE_URL}?key=${API_KEY}&${params}`;
-
-//    return fetch(url).then(response =>{
-//       if (response.ok) {
-//          return response.json();
-//        } else {
-//           throw new Error(response.status);
-//        }
-//    });
-// }
-
 function renderGalleryCard(value) {
-   // console.log(value);
-   if (value.hits.length === 0) {
-      return Notify.info('Sorry, there are no images matching your search query. Please try again.');
-   }
-   Notify.success(`Hooray! We found ${value.totalHits} images.`)
-   galleryContainerRef.innerHTML = '';
-   const markup = galleryCard(value.hits);
-   galleryContainerRef.innerHTML = markup;
-   btnLoad.classList.remove('visually-hidden');
+  // console.log(value);
+  if (value.hits.length === 0) {
+    return Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  const markup = galleryCard(value.hits);
+  galleryContainerRef.insertAdjacentHTML('beforeend', markup);
+  btnLoad.classList.remove('visually-hidden');
 }
 
 function onSearchError(error) {
-   Notify.failure(error);
+  Notify.failure(error);
 }
 
-btnLoad.addEventListener('click', ()=>{
-   ImagesAPI.findImages().then(renderGalleryCard).catch(onSearchError);
-})
+btnLoad.addEventListener('click', () => {
+  ImagesAPI.findImages().then(renderGalleryCard).catch(onSearchError);
+});
 
 let lightbox = new SimpleLightbox('.gallery a');
 
+   // function findImages(searchQuery) {
+   //    const BASE_URL = 'https://pixabay.com/api/';
+   //    const API_KEY = '31422890-5e40c603f0e6080de62657891';
+   //    const params = new URLSearchParams({
+   //       'q': searchQuery,
+   //       'image_type': 'photo',
+   //       'orientation': 'horizontal',
+   //       'safesearch': 'true',
+   //       'per_page': 40,
+   //       'page': 1
+   //    });
+   
+   //    const url = `${BASE_URL}?key=${API_KEY}&${params}`;
+   
+   //    return fetch(url).then(response =>{
+   //       if (response.ok) {
+   //          return response.json();
+   //        } else {
+   //           throw new Error(response.status);
+   //        }
+   //    });
+   // }
--- src/js/getImages.js
+++ src/js/getImages.js
@@ -3,21 +3,18 @@ const API_KEY = '31422890-5e40c603f0e6080de62657891';
 
 export class ImagesAPI {
    static currentPage = 1;
-   static searchQuery = '';
+   static query = '';
 
    static findImages(searchQuery) {
       if (searchQuery !== undefined) {
-         ImagesAPI.searchQuery = searchQuery;
+         ImagesAPI.query = searchQuery;
          ImagesAPI.currentPage = 1;
       } else {
          ImagesAPI.currentPage += 1;
       }
 
-
-      // const BASE_URL = 'https://pixabay.com/api/';
-      // const API_KEY = '31422890-5e40c603f0e6080de62657891';
       const params = new URLSearchParams({
-         'q': ImagesAPI.searchQuery,
+         'q': ImagesAPI.query,
          'image_type': 'photo',
          'orientation': 'horizontal',
          'safesearch': 'true',
